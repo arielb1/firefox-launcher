2c543c9a794861b06864507452cbfcb9a6edbf70
arielb1@mail.tau.ac.il <arielb1@localhost.localdomain>
2013-12-01 16:57:51 +0200

    Began refactoring updater

---
+++ lfx/updater.py
@@ -0,0 +1,67 @@
+import sys, hashlib
+
+from bz2 import BZ2Decompressor
+
+# FIXME
+from bz2 import (BZ2Decompressor as Decompressor,
+                   BZ2Compressor as Compressor)
+
+from . import mozilla
+from .versionfile import VersionFile
+from .filekit import AtomicReplacement
+
+def display_asterisk():
+    sys.stderr.write('*')
+    sys.stderr.flush()
+
+def try_update_firefox(temp_ctx, lock_name, arc_name,
+                       check_interval, gnupg_dir, before_update):
+    with VersionFile(lock_name, mozilla.FirefoxVersion,
+                     check_interval) as vers:
+        time_to_next = vers.can_skip_updates()
+        if time_to_next:
+            return time_to_next
+
+        print('[-] Checking Firefox Version...', end=' ', file=sys.stderr)
+        latest = mozilla.get_latest_firefox_version()
+        print(latest)
+
+        if vers.register_update(latest):
+            before_update()
+            with AtomicReplacement(arc_name, temp_ctx) as out:
+                print('[+] Updating Firefox', file=sys.stderr)
+                update_firefox(version, out, gnupg_dir)
+                out.ready = True
+
+def update_firefox(version, out, gnupg_dir):
+    bz2_archive = get_bz2_archive(version, gnupg_dir)
+    print('[-] Converting & Storing...', end=' ', file=sys.stderr)
+    write_fx_archive(bz2_archive, out)
+    print('Done', file=sys.stderr)
+
+def get_bz2_archive(version, gnupg_dir):
+    algo, digest = mozilla.get_firefox_hash(version, gnupg_dir)
+    scanner = hashlib.new(algo)
+
+    firefox_bz2 = mozilla.get_firefox_bz2(version, display_asterisk)
+    scanner.update_firefox_bz2()
+    if scanner.hexdigest() != digest:
+        raise ValueError('Hash Verification Failure', scanner.hexdigest(),
+                         digest)
+
+    return firefox_bz2
+
+BLOCK_SIZE = 1048576
+def write_fx_archive(firefox_bz2, out):
+    decom = BZ2Decompressor()
+    comp = Compressor()
+
+    pos = 0
+    while pos < len(firefox_bz2):
+        out.write(comp.compress(decom.decompress(
+                    firefox_bz2[pos:pos+BLOCK_SIZE])))
+
+        display_asterisk()
+        pos += BLOCK_SIZE
+
+    out.write(comp.flush())
---
+++ lfx/versionfile.py
@@ -0,0 +1,45 @@
+from .filekit import LockFile
+import time
+
+class VersionFile:
+      def __init__(self, fn, version_parser, check_interval):
+            self.fn, self.check_interval = fn, check_interval
+            self.version_parser = version_parser
+            self.version = self.check_time = None
+            self.lockf = None
+
+      def __enter__(self):
+            self.lockf = LockFile(self.fn, True).__enter__()
+            return self
+
+      def __exit__(self, et, ex, tb):
+            if self.check_time is not None and ex is None:
+                  self.lockf.setvalue(b'%s %s\n'.format(self.check_time,
+                                      self.version.encode('utf-8')))
+            return self.lockf.__exit__(et, ex, tb)
+
+      def can_skip_updates(self):
+            '''\
+Returns falsey if updates can be skipped, or number of seconds before
+    next update otherwise.
+
+Must be called before receive_update.
+'''
+            time_s, version_s = self.lockf().read().decode(
+                  'utf-8').strip().split(' ')
+            cur_time = time.time()
+            if cur_time - float(time_s) < check_interval:
+                  return cur_time - float(time_s)
+            self.version = version_s
+            self.check_time = cur_time
+            return 0
+
+      def register_update(self, newversion):
+            '''\
+Returns True if the new version is an update, False otherwise.
+Also updates the version saved on scope exit.
+'''
+            if newversion > self.version_parser(self.version):
+                  self.version = newversion
+                  return True
+            return False
